45bae08e3257679ff6f157337f50f0611f9dd45a
FaureWu <faure@wis.design>
2025-01-24 10:19:28 +0800

    feat: add new icons

---
+++ dist/IfFilledIcon.d.ts
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+declare const IfFilledIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+export default IfFilledIcon;
